55383c469885510597672b0fe04812f1bd9962fc
Henrique Silva <developer.henrique@hotmail.com>
2020-04-21 12:48:55 -0300

    Padrao de projeto prototype e limpexa cache para gitignore

---
+++ Prototype/LivroPhpPrototype.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace Prototype;
+
+
+class LivroPhpPrototype extends LivroPrototype
+{
+    public function __construct()
+    {
+        $this->setAssunto('PHP');
+    }
+
+    public function __clone()
+    {
+        echo "Livro PHP Clonado\n";
+    }
+}
---
+++ Prototype/LivroPrototype.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Prototype;
+
+abstract class LivroPrototype
+{
+    protected $titulo;
+    protected $assunto;
+    protected $nomeTitular;
+
+    abstract public function __clone();
+
+    public function getTitutlo(): string
+    {
+        return $this->titulo;
+    }
+
+    public function setTitulo(string $titulo): LivroPrototype
+    {
+        $this->titulo = $titulo;
+        return $this;
+    }
+
+    public function getAssunto(): string
+    {
+        return $this->assunto;
+    }
+
+    public function setAssunto(string $assunto): LivroPrototype
+    {
+        $this->assunto = $assunto;
+        return $this;
+    }
+
+    public function getNomeTitular()
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNomeTitular($nomeTitular): LivroPrototype
+    {
+        $this->nomeTitular = $nomeTitular;
+        return $this;
+    }
+}
